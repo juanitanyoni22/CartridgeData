efc011d387d707011289888b29c438929d51395f
mackawara <84657473+mackawara@users.noreply.github.com>
2022-06-19 22:57:39 +0200

    feat: added mongoose boilerplate

--- app.js
+++ app.js
@@ -2,3 +2,27 @@ const express=require("express"),
 bodyparser=require("body-parser"),
 app=express().use(bodyparser.urlencoded())
 
+const mongoose = require("mongoose");
+
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
+const databaseName = "cartridges";
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbname: databaseName,
+};
+const uri =
+  process.env
+    .MONGODB_URI; /* process.env.MONGODB_URI || process.env.MONGOLAB_URI || process.env.MONGOLHQ_URI  */
+
+let connection = mongoose.connect(uri || process.env.DB_URI, options);
+
+const database = mongoose.connection;
+database.on("error", console.error.bind(console, "connection error:"));
+database.once("open", async function () {
+  //wait for db to connect before running server
+  console.log(`DAtabase connection established  and checking`);
+
+})
