5d2a1cd642ece9ca369965ee4c0f75511af1a55d
mackawara <84657473+mackawara@users.noreply.github.com>
2022-06-20 16:52:05 -0600

    feat: added fetch and response handling logic

--- public/upload.js
+++ public/upload.js
@@ -14,4 +14,25 @@ window.addEventListener("DOMContentLoaded", () => {
     console.log("submit clicked");
     console.log(formData);
   });
+  const sendform = async (data) => {
+    const result = document.getElementById("result");
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      mode: "cors",
+      redirect: "follow",
+      body: JSON.stringify(formData),
+    };
+    fetch("/upload", options)
+      .then((response) => {
+        const resObj = JSON.parse(response);
+        console.log(response);
+        for (const any in resObj) {
+          document.createElement("p").innerText = any;
+        }
+      })
+      .catch((err) => {});
+  };
 });
