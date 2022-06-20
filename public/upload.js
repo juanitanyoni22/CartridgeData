7cba7b0d9707a8c628213df994e460b9f7bf7fb9
mackawara <84657473+mackawara@users.noreply.github.com>
2022-06-20 16:21:38 -0600

    feat: added event listeners to form

--- public/upload.js
+++ public/upload.js
@@ -1,7 +1,14 @@
 window.addEventListener("DOMContentLoaded", () => {
   console.log("content loaded");
   const submit = document.querySelector("#submit");
-  submit.addEventListener("click", () => {
+  const form = document.querySelector("#myForm");
+  const inputs = form.querySelectorAll("input");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    inputs.forEach((input) => {
+      console.log(input.value);
+    });
     console.log("submit clicked");
   });
 });
