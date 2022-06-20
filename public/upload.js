0ffb60743aadb84097b9556d524722339d00875d
mackawara <84657473+mackawara@users.noreply.github.com>
2022-06-20 16:30:38 -0600

    fix: changed data capturing logic

--- public/upload.js
+++ public/upload.js
@@ -2,12 +2,14 @@ window.addEventListener("DOMContentLoaded", () => {
   console.log("content loaded");
   const submit = document.querySelector("#submit");
   const form = document.querySelector("#myForm");
-  const inputs = form.querySelectorAll("input");
+  const inputs = form.querySelectorAll(".data");
+  let formData={}
 
-  form.addEventListener("submit", (e) => {
+  form.addEventListener("submit",async (e) => {
     e.preventDefault();
     inputs.forEach((input) => {
       console.log(input.value);
+
     });
     console.log("submit clicked");
   });
