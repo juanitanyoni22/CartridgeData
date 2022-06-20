cccc8d27b232b38de8de0da2244bb2f6b0b09dbc
mackawara <84657473+mackawara@users.noreply.github.com>
2022-06-20 16:40:33 -0600

    feat: added upload related logic for capturing data

--- public/upload.js
+++ public/upload.js
@@ -3,14 +3,15 @@ window.addEventListener("DOMContentLoaded", () => {
   const submit = document.querySelector("#submit");
   const form = document.querySelector("#myForm");
   const inputs = form.querySelectorAll(".data");
-  let formData={}
+  let formData = {};
 
-  form.addEventListener("submit",async (e) => {
+  form.addEventListener("submit", async (e) => {
     e.preventDefault();
     inputs.forEach((input) => {
       console.log(input.value);
-
+      formData[input.name] = input.value;
     });
     console.log("submit clicked");
+    console.log(formData);
   });
 });
